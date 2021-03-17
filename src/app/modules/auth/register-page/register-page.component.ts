3d52135c6a2f9726d3c83b8b6c83979bf98f890e
TakeYouWanna <stepankovarty@gmail.com>
2021-03-17 19:06:07 +0300

    create register-page.component

---
+++ src/app/modules/auth/register-page/register-page.component.ts
@@ -0,0 +1,39 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { createUser } from 'src/app/store/user/user.actions';
+import { matchValues } from './validators/match-values.validator';
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegisterPageComponent implements OnInit {
+  public registerForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private store$: Store) {}
+
+  public ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+      passwordConfirm: [
+        null,
+        [Validators.required, Validators.minLength(6), matchValues('password')],
+      ],
+    });
+  }
+
+  public onSubmit(): void {
+    const { email, password } = this.registerForm.value;
+    this.store$.dispatch(createUser({ email, password }));
+    this.registerForm.reset();
+  }
+}
