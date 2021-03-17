a2dc90ce5e152d045cb0904081099a6a2a489e0f
TakeYouWanna <stepankovarty@gmail.com>
2021-03-17 19:09:43 +0300

    update app.module and app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { User } from './shared/interfaces/user.interface';
+import { initializeUser, loadUserSuccess } from './store/user/user.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Innowise-lab-internship-level2';
+
+  constructor(private store$: Store<User>) {
+    this.store$.dispatch(initializeUser());
+  }
+
+  public ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from 'src/environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './shared/components/top-bar/top-bar.component';
+import { userReducer } from './store/user/user.reducer';
+import { UserEffect } from './store/user/user.effect';
+import { toastNoticeReducer } from './store/toast-notice/toast-notice.reducer';
+import { ToastNoticeEffect } from './store/toast-notice/toast-notice.effect';
+import { ToastNoticeComponent } from './shared/components/toast-message/toast-notice.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TopBarComponent, ToastNoticeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    StoreModule.forRoot({ user: userReducer, toastNotice: toastNoticeReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([UserEffect, ToastNoticeEffect]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
