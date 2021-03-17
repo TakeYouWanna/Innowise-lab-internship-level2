62d6cefa5bdfd2e3a00c887b6100657d35eb63e7
TakeYouWanna <stepankovarty@gmail.com>
2021-03-17 19:05:50 +0300

    create login-page.component

---
+++ src/app/modules/auth/login-page/login-page.component.ts
@@ -0,0 +1,30 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { User } from 'src/app/shared/interfaces/user.interface';
+import { loadUser } from 'src/app/store/user/user.actions';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginPageComponent implements OnInit {
+  public loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private store$: Store<User>) {}
+
+  public ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  public onSubmit(): void {
+    const { email, password } = this.loginForm.value;
+    this.store$.dispatch(loadUser({ email, password }));
+    this.loginForm.reset();
+  }
+}
