0ef0bb8074f37220b73762192ccac6b02b6f5ce6
TakeYouWanna <stepankovarty@gmail.com>
2021-03-17 19:06:27 +0300

    create validator for register-page.component

---
+++ src/app/modules/auth/register-page/validators/match-values.validator.ts
@@ -0,0 +1,12 @@
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+export const matchValues = (controlName: string): ValidationErrors | null => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    return !!control.value &&
+      control.parent?.value &&
+      control.value === control.parent.get(controlName)?.value
+      ? null
+      : { mismatch: true };
+  };
+};
