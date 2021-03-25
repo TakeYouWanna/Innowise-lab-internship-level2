1444660b6792b3ae6ac461f1b8b3c644f5a05e19
TakeYouWanna <stepankovarty@gmail.com>
2021-03-25 22:33:29 +0300

    updated top-bar component

--- src/app/shared/components/top-bar/top-bar.component.ts
+++ src/app/shared/components/top-bar/top-bar.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { logOutUser } from 'src/app/store/user/user.actions';
-import { selectUserUid } from 'src/app/store/user/user.selectors';
+import { State } from 'src/app/store';
+import { logOutUser } from 'src/app/store/user/actions';
+import { selectUserUid } from 'src/app/store/user/selectors';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TopBarComponent {
   public uid$: Observable<string> = this.store$.pipe(select(selectUserUid));
 
-  constructor(private store$: Store) {}
+  constructor(private store$: Store<State>, private router: Router) {}
 
   public logOut(): void {
     this.store$.dispatch(logOutUser());
+    this.uid$.subscribe((uid) => {
+      if (!uid) {
+        this.router.navigate(['']);
+      }
+    });
   }
 }
